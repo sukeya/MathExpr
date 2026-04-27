0668d5528874f429290c1eceff5f86d235a0f689
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2026-04-28 07:52:58 +0900

    Add lisence.

--- include/math_expr.hpp
+++ include/math_expr.hpp
@@ -28,6 +28,21 @@
  * (12) (x ilike 's*ri?g') and [y < (3 z^7 + w)]                  *
  *                                                                *
  ******************************************************************
+
+
+Copyright 2026 Yuya Asano <my_favorite_theory@yahoo.co.jp>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 #ifndef INCLUDE_MATH_EXPR_HPP
--- include/math_expr/details/node_utils.hpp
+++ include/math_expr/details/node_utils.hpp
@@ -1,3 +1,36 @@
+/*
+ **************************************************************
+ *         C++ Mathematical Expression Toolkit Library        *
+ *                                                            *
+ * Simple Example 02                                          *
+ * Author: Arash Partow (1999-2025)                           *
+ * URL: https://www.partow.net/programming/exprtk/index.html  *
+ *                                                            *
+ * Copyright notice:                                          *
+ * Free use of the Mathematical Expression Toolkit Library is *
+ * permitted under the guidelines and in accordance with the  *
+ * most current version of the MIT License.                   *
+ * https://www.opensource.org/licenses/MIT                    *
+ * SPDX-License-Identifier: MIT                               *
+ *                                                            *
+ **************************************************************
+
+
+Copyright 2026 Yuya Asano <my_favorite_theory@yahoo.co.jp>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #ifndef MATH_EXPR_DETAILS_NODE_UTILS_HPP
 #define MATH_EXPR_DETAILS_NODE_UTILS_HPP
 
